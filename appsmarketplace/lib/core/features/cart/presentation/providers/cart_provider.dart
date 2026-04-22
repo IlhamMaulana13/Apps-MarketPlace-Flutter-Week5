8d94bd25d6fe78f9272dc120056937723def447e
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-04-22 13:20:01 +0700

    menambah clas cart provider

--- appsmarketplace/lib/core/features/cart/presentation/providers/cart_provider.dart
+++ appsmarketplace/lib/core/features/cart/presentation/providers/cart_provider.dart
@@ -1,2 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:appsmarketplace/core/services/dio_client.dart';
+
+class CartProvider extends ChangeNotifier {
+  List items = [];
+  double totalPrice = 0;
+  bool isLoading = false;
