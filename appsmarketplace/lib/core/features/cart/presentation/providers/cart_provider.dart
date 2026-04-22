937585c4b15f1413aeb1b8baad9203e299c31ca0
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-04-22 13:20:21 +0700

    GET CART

--- appsmarketplace/lib/core/features/cart/presentation/providers/cart_provider.dart
+++ appsmarketplace/lib/core/features/cart/presentation/providers/cart_provider.dart
@@ -5,3 +5,17 @@ class CartProvider extends ChangeNotifier {
   List items = [];
   double totalPrice = 0;
   bool isLoading = false;
+
+Future<void> fetchCart() async {
+    isLoading = true;
+    notifyListeners();
+
+    final res = await DioClient.instance.get('/cart');
+
+    items = res.data['data']['items'] ?? [];
+    totalPrice =
+        (res.data['data']['total_price'] as num?)?.toDouble() ?? 0;
+
+    isLoading = false;
+    notifyListeners();
+  }
