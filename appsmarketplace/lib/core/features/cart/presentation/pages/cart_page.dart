605e3262a006f69de2f172c33d036523e7812c54
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-04-22 13:24:39 +0700

    membuat class cart page

--- appsmarketplace/lib/core/features/cart/presentation/pages/cart_page.dart
+++ appsmarketplace/lib/core/features/cart/presentation/pages/cart_page.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:appsmarketplace/core/features/cart/presentation/providers/cart_provider.dart';
+
+class CartPage extends StatefulWidget {
+  const CartPage({super.key});
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
