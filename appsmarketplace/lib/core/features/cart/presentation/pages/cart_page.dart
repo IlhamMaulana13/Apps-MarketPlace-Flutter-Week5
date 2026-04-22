025f0b5629f064805117ff854418b2ee5907dc71
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-04-22 13:24:59 +0700

    membuat class cart page State

--- appsmarketplace/lib/core/features/cart/presentation/pages/cart_page.dart
+++ appsmarketplace/lib/core/features/cart/presentation/pages/cart_page.dart
@@ -8,3 +8,13 @@ class CartPage extends StatefulWidget {
   @override
   State<CartPage> createState() => _CartPageState();
 }
+
+class _CartPageState extends State<CartPage> {
+  String formatPrice(num price) {
+    return price
+        .toStringAsFixed(0)
+        .replaceAllMapped(
+          RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
+          (m) => '${m[1]}.',
+        );
+  }
